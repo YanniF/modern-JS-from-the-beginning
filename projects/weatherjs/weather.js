de996630d4138d4942427a3666268b388bfb393b
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-03-28 18:40:10 -0300

    Weather JS

---
+++ projects/weatherjs/weather.js
@@ -0,0 +1,21 @@
+class Weather {
+  constructor(city, state) {
+    this.apiKey = 'bdcd325e81a65b2b';
+    this.city = city;
+    this.state = state;
+  }
+
+  // fetch weather from API
+  async getWeather() {
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+    const responseData = await response.json();
+
+    return responseData.current_observation;
+  }
+
+  // change weather location
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
